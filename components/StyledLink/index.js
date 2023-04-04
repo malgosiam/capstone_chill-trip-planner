e26113f6cbcb514495a3602c226c4ab142b038c8
Malgorzata Wawrzyniak <malgosia.archi@gmail.com>
2023-04-04 21:39:29 +0200

    add link on homepage

---
+++ components/StyledLink/index.js
@@ -0,0 +1,10 @@
+import styled, { css } from "styled-components";
+
+export const StyledLink = styled.a`
+  background-color: var(--blue);
+  padding: 1rem;
+  border-radius: 1rem;
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+`;
