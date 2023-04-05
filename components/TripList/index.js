a1fd63d5a053c2033348f685724d93090d1b8bd8
Malgorzata Wawrzyniak <malgosia.archi@gmail.com>
2023-04-05 11:08:55 +0200

    Update components/TripList/index.js
    Co-authored-by: Jessica Lörs <54777853+JessicaLoers@users.noreply.github.com>

--- components/TripList/index.js
+++ components/TripList/index.js
@@ -2,17 +2,18 @@ import StyledListContainer from "../StyledListContainer";
 import StyledListItem from "../StyledListItem";
 
 export default function TripList({ trips }) {
+ const sortedTrips = trips
+    ?.slice()
+    .sort((a, b) => new Date(b.startDate) - new Date(a.startDate));
+
   return (
     <StyledListContainer>
-      {trips
-        ?.slice()
-        .sort((a, b) => new Date(b.startDate) - new Date(a.startDate))
-        .map((trip) => (
-          <StyledListItem key={trip.id}>
-            <time>{trip.startDate}</time>
-            {trip.townName}
-          </StyledListItem>
-        ))}
+      {sortedTrips.map((trip) => (
+        <StyledListItem key={trip.id}>
+          <time>{trip.startDate}</time>
+          {trip.townName}
+        </StyledListItem>
+      ))}
     </StyledListContainer>
   );
 }
