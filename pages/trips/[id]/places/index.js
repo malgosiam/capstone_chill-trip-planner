75d741cce04a2a66b12f32574604972e64c4bc5e
Malgorzata Wawrzyniak <malgosia.archi@gmail.com>
2023-04-23 23:21:12 +0200

    extract and move StyledCard component to the components folder

--- pages/trips/[id]/places/index.js
+++ pages/trips/[id]/places/index.js
@@ -6,44 +6,7 @@ import styled from "styled-components";
 import Link from "next/link";
 import NotChillSvg from "../../../../public/assets/chicken1.svg";
 import ChillSvg from "../../../../public/assets/chicken2_1.svg";
-
-const StyledCard = styled.article`
-  position: relative;
-  width: 85vw;
-  display: flex;
-  flex-direction: column;
-  background-color: var(--blue);
-  background: linear-gradient(90deg, var(--blue) 35%, var(--light-blue) 100%);
-  color: var(--white);
-  padding: 1rem;
-  margin: 0.5rem;
-  border-radius: 1.3rem;
-  border: 1px solid var(--light-blue);
-  box-shadow: 0 0 10px var(--shadow);
-
-  h3 {
-    margin-right: 4.8rem;
-    font-weight: 600;
-  }
-
-  fieldset {
-    border: 1px solid var(--yellow);
-    border-radius: 12px;
-  }
-
-  legend {
-    color: var(--yellow);
-    text-shadow: 1px 1px 1px var(--blue);
-    font-weight: 600;
-    margin-right: 0.5rem;
-  }
-
-  p {
-    margin: 0.3rem;
-    text-align: justify;
-    text-justify: distribute;
-  }
-`;
+import StyledCard from "../../../../components/StyledCard";
 
 const StyledBox = styled.section`
   display: flex;
